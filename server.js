5bee84d343bd9bb243443db1d658e61db60dd1d6
akhunov43-dev <akhunov43@gmail.com>
2026-08-27 14:01:07 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const express = require("express");
 const app = express();
 const http = require("http")
 
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
 // 1 bosqich - Kirish kodlar - public folder ochiq (css - imagesni bu yerga joylashtirish mumkin)
 app.use(express.static("public"));
 app.use(express.json());
@@ -35,7 +46,11 @@ res.json({test: "success"});
 
 app.get("/", function (req, res) {
     res.render('harid');
-})
+});
+
+app.get('/author', (req, res) => {
+    res.render("author", {user: user});
+});
 
 
 
