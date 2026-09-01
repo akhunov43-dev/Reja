045960b25924cc3b35970684c6cd374e68eddc1e
akhunov43-dev <akhunov43@gmail.com>
2026-09-01 17:44:26 +0900

    feat: mongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,63 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+//Mongodb chaqirish - mongodb objectini olib beradi-datbasega malumotlarni yozish va o'qish uchun
+// const db = require("./server").db();
+
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
+
+
+
+
+// 1 bosqich - Kirish kodlar - public folder ochiq (css - imagesni bu yerga joylashtirish mumkin)
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+//2 - session codes
+
+//3 - view codes - backendni ichida html, frontend ni yasaymiz
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+
+//4 - Routing codes
+/* 
+app.get("/hello", function(req, res) {
+    res.end(`<h1 style="background: red">HELLO WORLD<h1>`);
+});
+
+
+app.get("/gift", function(req, res) {
+    res.end(`<h1 style="background: red">Siz sovgalar sahifasidasiz<h1>`);
+});
+
+*/
+app.post("/create-item", (req, res) => {
+console.log(req.body);
+res.json({test: "success"});
+});
+
+app.get("/", function (req, res) {
+    res.render('reja');
+});
+
+app.get('/author', (req, res) => {
+    res.render("author", {user: user});
+});
+
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,62 +1,35 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
-const http = require("http")
+const http = require("http");
+const app = require("./app");
+const { MongoClient } = require("mongodb");
 
-const fs = require("fs");
+const connectionString =
+  "mongodb+srv://akhunov43_db_user:rqsEfyORCqdoCQH9@cluster0.zctnulj.mongodb.net/Reja?retryWrites=true&w=majority";
 
-let user;
-fs.readFile("database/user.json", "utf-8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
+MongoClient.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) {
+      console.log("ERROR: on connection to MongoDB");
+      console.log(err);
+      process.exit(1);
     } else {
-        user = JSON.parse(data)
+      console.log("MongoDB connection succeed");
+
+     module.exports = client;
+     
+      const db = client.db("Reja");
+      
+
+      const server = http.createServer(app);
+      const PORT = 3000;
+
+      server.listen(PORT, function () {
+        console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+      });
     }
-});
-
-// 1 bosqich - Kirish kodlar - public folder ochiq (css - imagesni bu yerga joylashtirish mumkin)
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-
-//2 - session codes
-
-//3 - view codes - backendni ichida html, frontend ni yasaymiz
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-
-//4 - Routing codes
-/* 
-app.get("/hello", function(req, res) {
-    res.end(`<h1 style="background: red">HELLO WORLD<h1>`);
-});
-
-
-app.get("/gift", function(req, res) {
-    res.end(`<h1 style="background: red">Siz sovgalar sahifasidasiz<h1>`);
-});
-
-*/
-app.post("/create-item", (req, res) => {
-console.log(req.body);
-res.json({test: "success"});
-});
-
-app.get("/", function (req, res) {
-    res.render('reja');
-});
-
-app.get('/author', (req, res) => {
-    res.render("author", {user: user});
-});
-
-
-
-
-const server = http.createServer(app);
-let PORT = 3000
-server.listen(PORT, function(){
-    console.log(`The server is running succesfully working on port: ${PORT}, http://localhost:${PORT}`);
-});
+  }
+);
